69b4cfd49909d9fd609a3f2d8534f222c5282487
Jan Popelas <janopopelas@gmail.com>
2020-12-08 19:21:34 +0100

    update day 2

--- day02/password_philosophy.py
+++ day02/password_philosophy.py
@@ -20,6 +20,11 @@ def verify_pass(password: Dict) -> bool:
     return password["min"] <= len([1 for i in password["password"] if i == password["req_char"]]) <= password["max"]
 
 
+def part_one(data_dict: List[Dict]) -> None:
+    print(f"There are {len([1 for i in data_dict if verify_pass(i)])} valid passwords out of "
+          f"{len(data_dict)} for part1")
+
+
 def verify_pass2(password: Dict) -> bool:
     res = 0
     for phr in ["min", "max"]:
@@ -28,14 +33,16 @@ def verify_pass2(password: Dict) -> bool:
     return res == 1
 
 
-def print_results():
-    data_dict = input_to_obj("input_data.txt")
-    print(f"There are {len([1 for i in data_dict if verify_pass(i)])} valid passwords out of "
-          f"{len(data_dict)} for part1")
-
+def part_two(data_dict: List[Dict]) -> None:
     print(f"There are {len([1 for i in data_dict if verify_pass2(i)])} valid passwords out of "
           f"{len(data_dict)} for part2")
 
 
+def main():
+    data_dict = input_to_obj("input_data.txt")
+    part_one(data_dict)
+    part_two(data_dict)
+
+
 if __name__ == '__main__':
-    print_results()
+    main()
