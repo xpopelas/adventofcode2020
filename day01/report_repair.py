b39eb43f0f07f35072e6ef87ce1286c0ac4f2b0e
Jan Popelas <janopopelas@gmail.com>
2020-12-08 19:21:26 +0100

    update day 1

--- day01/report_repair.py
+++ day01/report_repair.py
@@ -26,16 +26,23 @@ def input_file_to_list(input_path: str) -> List[int]:
     return result
 
 
-def calc_numbers():
-    input_list = input_file_to_list("input_data.txt")
+def part_one(input_list: List[int]) -> None:
     two_numbers = find_to_sum(input_list, 2020)
     print(f"The two numbers are {two_numbers[0]} and {two_numbers[1]}")
     print(f"Answer to part1 is {two_numbers[0] * two_numbers[1]}")
 
+
+def part_two(input_list: List[int]) -> None:
     three_numbers = find_three_sum(input_list, 2020)
     print(f"The three numbers are {three_numbers[0]}, {three_numbers[1]} and {three_numbers[2]}")
     print(f"Answer to part2 is {three_numbers[0] * three_numbers[1] * three_numbers[2]}")
 
 
+def main() -> None:
+    input_list = input_file_to_list("input_data.txt")
+    part_one(input_list)
+    part_two(input_list)
+
+
 if __name__ == '__main__':
-    calc_numbers()
+    main()
